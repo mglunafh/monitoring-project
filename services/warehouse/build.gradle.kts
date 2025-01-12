3c65a4c262b9bf41bf21a8f3c7dbc9559033e959
ufispb <ufispb@gmail.com>
2025-01-12 20:26:03 +0300

    IPMN-23 warehouse: added controller layer tests

--- services/warehouse/build.gradle.kts
+++ services/warehouse/build.gradle.kts
@@ -13,4 +13,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(kotlin("test"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.assertj:assertj-core")
 }
---
+++ services/warehouse/src/test/kotlin/org/burufi/monitoring/warehouse/controller/WarehouseControllerTest.kt
@@ -0,0 +1,93 @@
+package org.burufi.monitoring.warehouse.controller
+
+import org.burufi.monitoring.dto.warehouse.GoodsItemDto
+import org.burufi.monitoring.dto.warehouse.SupplierDto
+import org.burufi.monitoring.warehouse.service.WarehouseService
+import org.hamcrest.core.StringContains
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+import kotlin.test.Test
+
+@WebMvcTest(controllers = [WarehouseController::class])
+class WarehouseControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockitoBean
+    lateinit var warehouseService: WarehouseService
+
+    @Test
+    fun `Test list suppliers`() {
+        val testSuppliers = listOf(SupplierDto(id = 100, name = "TestSupplier", description = "Test Description"))
+        Mockito.doReturn(testSuppliers).`when`(warehouseService).getSuppliers()
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/warehouse/suppliers").accept(APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.responseCode").value("OK"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.suppliers[0].id").value(100))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.suppliers[0].name").value("TestSupplier"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.suppliers[0].description").value("Test Description"))
+    }
+
+    @Test
+    fun `Test list goods`() {
+        val item = GoodsItemDto(10, "Test instance", "TEST", 1, BigDecimal(180.5))
+        val testGoods = listOf(item)
+        Mockito.doReturn(testGoods).`when`(warehouseService).getGoods()
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/warehouse/goods").accept(APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.responseCode").value("OK"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.goods[0].id").value(item.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.goods[0].name").value(item.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.goods[0].category").value(item.category))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.goods[0].amount").value(item.amount))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.payload.goods[0].weight").value(item.weight))
+    }
+
+    @Test
+    fun `Test register contract`() {
+    }
+
+    @Test
+    fun `Test register contract, but no items in the request`() {
+        val badRequest = """
+            {"suppliedId": 100, "items": []}
+        """.trimIndent()
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/warehouse/contract")
+            .content(badRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.responseCode").value("VALIDATION_FAILURE"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.errorMessage")
+                .value("Field 'items': Contract must contain some items, got '[]' instead."))
+    }
+
+    @Test
+    fun `Test register contract, but some item is incorrect`() {
+        val badRequest = """
+            { "suppliedId": 100, "items": [ { "id": 15, "price": -10, "amount": -2 } ] }
+        """.trimIndent()
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/warehouse/contract")
+            .content(badRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.responseCode").value("VALIDATION_FAILURE"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.errorMessage")
+                .value(StringContains.containsString("Field 'items[0].amount': Amount of items to order from a supplier must be positive, got '-2' instead.")))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.errorMessage")
+                .value(StringContains.containsString("Field 'items[0].price': Item price must be positive, got '-10' instead.")))
+    }
+}
