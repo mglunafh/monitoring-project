b3d8bd9d211709b7acb9bbf9f5ae56cfb9512195
ufispb <ufispb@gmail.com>
2025-02-16 16:13:23 +0300

    added prometheus monitoring support into warehouse app

--- services/warehouse/build.gradle.kts
+++ services/warehouse/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
